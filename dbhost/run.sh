74bf20b73455c5e3dc09efa3b6e184ea5d7d7510
Juozas L <lizdenis.juozas@gmail.com>
2017-12-25 16:31:31 +0000

    dbhost fixes

--- dbhost/run.sh
+++ dbhost/run.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-# mongodb run
-exec /usr/bin/mongod --config /etc/mongod.conf
+set -e
 
-# mysql run
+# init mysql db if necessary
 if [ ! -d /var/lib/mysql/mysql ];then
     mysqld --initialize-insecure --user=root --datadir=/var/lib/mysql
 fi
 
 chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
 
-# snmpd run
-/etc/init.d/snmpd start
 
-# cron
-cron
-
-
-
+# super visor deamons start
+exec /usr/bin/supervisord --nodaemon -c /etc/supervisor/supervisord.conf

31ef86644cc17f2417cff68a3c3aef0692abab90
Juozas L <lizdenis.juozas@gmail.com>
2017-12-25 17:19:26 +0000

    dbhost fix

--- dbhost/run.sh
+++ dbhost/run.sh
@@ -7,7 +7,7 @@ if [ ! -d /var/lib/mysql/mysql ];then
     mysqld --initialize-insecure --user=root --datadir=/var/lib/mysql
 fi
 
-chown -R mysql:mysql /var/lib/mysql /var/run/mysqld
+chown -R mysql:mysql /var/lib/mysql
 
 # super visor deamons start
 exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
